6668c65787cc8f120c67c531f06c7b17327b1b4f
User <user@example.com>
2025-04-02 10:56:47 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/tools-collection/',
+  base: './',
   build: {
     assetsDir: 'assets',
     emptyOutDir: true,
